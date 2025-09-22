850fb230ebc1832bfd316beed5416f7a31bdfe5f
Soham Garg <63478296+sohamgarg2020@users.noreply.github.com>
2025-09-22 10:32:25 -0700

    Problem 958

---
+++ 958. Check Completeness of a Binary Tree/code.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        q = deque([root])
+        seen_null = False
+
+        while q:
+            node = q.popleft()
+            if node is None:
+                seen_null = True
+            else:
+                if seen_null:
+                    return False
+                q.append(node.left)
+                q.append(node.right)
+        return True
