036735a18ae273b8711e9b1c3be98aa570bc182a
Soham Garg <63478296+sohamgarg2020@users.noreply.github.com>
2025-09-27 10:16:50 -0700

    Problem 812

---
+++ 812. Largest Triangle Area/code.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        length = len(points)
+        area = 0
+        for i in range(length):
+            for j in range(i, length):
+                for k in range (j, length):
+                    x1 = points[i][0]
+                    x2 = points[j][0]
+                    x3 = points[k][0]
+
+                    y1 = points[i][1]
+                    y2 = points[j][1]
+                    y3 = points[k][1]
+
+                    area = max(abs(0.5 * (x1*(y2-y3) + x2*(y3 - y1) + x3*(y1-y2))), area)
+        return area
